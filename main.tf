efaa95b770d28b1da09e082d9ea62cfff88f90f3
lukas-camilo <lukscamilo@gmail.com>
2025-04-14 11:51:05 -0300

    Ajustando workflow

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_api_gateway_authorizer" "cognito_authorizer" {
 
 resource "aws_api_gateway_integration" "lambda_integration" {
     rest_api_id = aws_api_gateway_rest_api.fit_strike_api.id
-    resource_id = aws_api_gateway_rest_api.fit_strike_api.resource_id
+    resource_id = aws_api_gateway_rest_api.fit_strike_api.root_resource_id
     http_method = "ANY"
     integration_http_method = "POST"
     type = "AWS_PROXY"
